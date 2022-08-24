c0fc32f555f1d71fff3d62bfaab0e286ffd752eb
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-08-24 23:36:19 +0200

    add basket logo plus styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,14 @@
 import './Navbar.css';
 import { Search } from '../Search/Search';
+import basketLogo from './basket.svg';
 
-export function Navbar() { 
+export function Navbar() {
   return (
     <nav className="navbar">
       <Search />
+      <button className='cart-icon'>
+        <img src={ basketLogo } className="basket" alt="Basket Logo" />
+        </button>
     </nav>
   )
 }
