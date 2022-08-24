0abe75d2e599f0ec18d6880eff6c17d265682b50
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-08-24 19:42:50 +0200

    add Home and List pages add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { List } from './pages/List';
 
 function App() {
 
   return (
-    <h1>Events App</h1>
+    <>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/list' element={ <List />} />
+      </Routes>
+    </>
   )
 }
 
