5f14a26f02d3fc500885cfc99ac3242bf5343379
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-08-24 19:49:04 +0200

    add navbar and search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { List } from './pages/List';
+import { Home } from './pages/Home/Home';
+import { List } from './pages/List/List';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar />
     <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/list' element={ <List />} />
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,3 @@
+export function Home() { 
+  return <h1>Public Events</h1>
+}
